8762e44f6e770bf469feaf644b381af4c278669b
itsmesrishti <131491576+itsmesrishti@users.noreply.github.com>
2023-09-04 15:48:13 +0530

    Update script.js

--- checkout-page-master/script.js
+++ checkout-page-master/script.js
@@ -11,31 +11,31 @@ for (var i = 0; i < cartButtons.length; i++) {
         // for adding items to cart
         if (btnId.includes("plus")) {
             // select and store the current value of element present before the plus button element
-            var itemNum = Number(this.previousElementSibling.innerHTML);
+            var itemNum = Number(this.previousElementSibling.value);
             // add and store value to the item present before plus button element
             itemNum += 1;
-            this.previousElementSibling.innerHTML = itemNum;
+            this.previousElementSibling.value = itemNum;
 
             // select and store the discounted price associated with the item whose plus btn is being clicked 
             var price = Number((this.parentElement.previousElementSibling.querySelector(".discounted-price").innerHTML).slice(1,));
             // select and store the total price and remove the '$' sign present at the start and convert string into number
-            var totalPrice = Number(document.querySelector("#total-price").innerHTML.slice(1,));
+            var totalPrice = Number(document.querySelector("#total-price").value.slice(1,));
             // add the discounted price to the total price and change the value of total price
             totalPrice += price;
-            document.querySelector("#total-price").innerHTML = '$' + totalPrice.toFixed(2);
+            document.querySelector("#total-price").value = '$' + totalPrice.toFixed(2);
         };
 
         // for subtracting items from cart
         if (btnId.includes("minus")) {
-            var itemNum = Number(this.nextElementSibling.innerHTML);
+            var itemNum = Number(this.nextElementSibling.value);
             // since we don't want negative values if statement is used
             if (itemNum > 0) {
                 itemNum -= 1;
-                this.nextElementSibling.innerHTML = itemNum;
+                this.nextElementSibling.value = itemNum;
                 var price = Number((this.parentElement.previousElementSibling.querySelector(".discounted-price").innerHTML).slice(1,));
-                var totalPrice = Number(document.querySelector("#total-price").innerHTML.slice(1,));
+                var totalPrice = Number(document.querySelector("#total-price").value.slice(1,));
                 totalPrice -= price;
-                document.querySelector("#total-price").innerHTML = '$' + totalPrice.toFixed(2);
+                document.querySelector("#total-price").value = '$' + totalPrice.toFixed(2);
             }
         };
     });
